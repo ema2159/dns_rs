f227d2c07a807a4f49ec11aed67c168b57cdc6ad
Emmanuel Bustos <ema2159@gmail.com>
2023-04-12 19:13:42 +0300

    mod: Return UNKNOWN DNSRecord variant instead of error

--- dns_protocol/dns_packet/dns_record.rs
+++ dns_protocol/dns_packet/dns_record.rs
@@ -11,11 +11,17 @@ pub enum DNSRecord {
         addr: Ipv4Addr,
         ttl: u32,
     },
+    UNKNOWN {
+        domain: String,
+        record_type: u16,
+        data_len: u16,
+        ttl: u32,
+    },
 }
 
 #[derive(Debug, PartialEq)]
 pub struct DNSRecordPreamble {
-    pub domain: String,    // Variable length
+    pub domain: String,            // Variable length
     pub record_type: DNSQueryType, // 2 bytes
     pub class: u16,                // 2 bytes
     pub ttl: u32,                  // 4 bytes
@@ -25,23 +31,40 @@ pub struct DNSRecordPreamble {
 #[derive(Debug, PartialEq)]
 pub enum DNSQueryType {
     A,
+    UNKNOWN(u16),
 }
 
 impl DNSQueryType {
     fn from_num(code_num: u16) -> Result<DNSQueryType, DNSPacketErr> {
         match code_num {
             1 => Ok(DNSQueryType::A),
-            _ => Err(DNSPacketErr::UnknownQueryTypeErr(code_num)),
+            _ => Ok(DNSQueryType::UNKNOWN(code_num)),
         }
     }
 }
 
 impl DNSRecord {
-    fn parse_type_a(buffer: &mut DNSPacketBuffer, preamble: DNSRecordPreamble) -> Result<DNSRecord, DNSPacketErr> {
-        Ok(DNSRecord::A{
+    fn parse_type_a(
+        buffer: &mut DNSPacketBuffer,
+        preamble: DNSRecordPreamble,
+    ) -> Result<DNSRecord, DNSPacketErr> {
+        Ok(DNSRecord::A {
             domain: preamble.domain,
             addr: Ipv4Addr::from(buffer.read_u32()?),
-            ttl: preamble.ttl
+            ttl: preamble.ttl,
+        })
+    }
+
+    fn parse_type_unknown(preamble: DNSRecordPreamble) -> Result<DNSRecord, DNSPacketErr> {
+        let DNSQueryType::UNKNOWN(record_type_num) = preamble.record_type
+            else {
+                unreachable!()
+            };
+        Ok(DNSRecord::UNKNOWN {
+            domain: preamble.domain,
+            record_type: record_type_num,
+            data_len: preamble.len,
+            ttl: preamble.ttl,
         })
     }
 
@@ -59,6 +82,7 @@ impl DNSRecord {
         };
         let record = match preamble.record_type {
             DNSQueryType::A => Ok(DNSRecord::parse_type_a(buffer, preamble)?),
+            DNSQueryType::UNKNOWN(_) => Ok(DNSRecord::parse_type_unknown(preamble)?),
         }?;
 
         Ok(record)
