22d46f79ddb0201ec9a90be88399e063fb6e142f
Emmanuel Bustos <ema2159@gmail.com>
2023-05-07 21:17:06 +0300

    test: Add tests for CNAME records

--- src/dns_packet/dns_record/cname_record.rs
+++ src/dns_packet/dns_record/cname_record.rs
@@ -36,3 +36,65 @@ impl DNSRecordDataWrite for CNAME {
         DNSQueryType::CNAME
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::{DNSDomain, DNSRecord, DNSRecordData, HEADER_SIZE};
+    use super::*;
+
+    #[test]
+    fn test_read_cname() {
+        let dns_packet_data = [
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x62,
+            0x61, 0x72, 0x07, 0x65, 0x78, 0x61, 0x6D, 0x70, 0x6C, 0x65, 0x03, 0x63, 0x6F, 0x6D,
+            0x00, 0x00, 0x05, 0x00, 0x01, 0x00, 0x00, 0x00, 0xFE, 0x00, 0x06, 0x03, 0x66, 0x6F,
+            0x6F, 0xC0, 0x10,
+        ];
+
+        let mut dns_packet_buffer = DNSPacketBuffer::new(&dns_packet_data);
+        dns_packet_buffer.seek(HEADER_SIZE);
+
+        let parsed_record = DNSRecord::parse_from_buffer(&mut dns_packet_buffer).unwrap();
+
+        let expected_record = DNSRecord::new(
+            DNSDomain("bar.example.com".to_string()),
+            1,
+            254,
+            DNSRecordData::CNAME(CNAME {
+                value: DNSDomain("foo.example.com".to_string()),
+            }),
+        );
+
+        assert_eq!(parsed_record.preamble.len, 6);
+        assert_eq!(parsed_record, expected_record);
+    }
+
+    #[test]
+    fn test_write_cname() {
+        let a_record = DNSRecord::new(
+            DNSDomain("bar.example.com".to_string()),
+            1,
+            254,
+            DNSRecordData::CNAME(CNAME {
+                value: DNSDomain("foo.example.com".to_string()),
+            }),
+        );
+
+        let mut buffer = DNSPacketBuffer::new(&[]);
+        buffer.seek(HEADER_SIZE);
+        a_record.write_to_buffer(&mut buffer).unwrap();
+
+        // Expected
+        let expected_data = [
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x62,
+            0x61, 0x72, 0x07, 0x65, 0x78, 0x61, 0x6D, 0x70, 0x6C, 0x65, 0x03, 0x63, 0x6F, 0x6D,
+            0x00, 0x00, 0x05, 0x00, 0x01, 0x00, 0x00, 0x00, 0xFE, 0x00, 0x06, 0x03, 0x66, 0x6F,
+            0x6F, 0xC0, 0x10,
+        ];
+
+        let mut expected_buffer = DNSPacketBuffer::new(&expected_data);
+        expected_buffer.seek(expected_data.len());
+
+        assert_eq!(buffer.get_data(), expected_buffer.get_data())
+    }
+}
