07d20d5c71fbbd5522b31b185612b73f1f5da104
Emmanuel Bustos <ema2159@gmail.com>
2023-05-21 21:04:29 +0300

    feat: Implement error for DNSError type

--- src/dns_packet/dns_packet_err.rs
+++ src/dns_packet/dns_packet_err.rs
@@ -1,5 +1,6 @@
 use super::QueryType;
-// use std::error::Error;
+use std::error::Error;
+use std::fmt;
 
 #[derive(Debug, PartialEq)]
 pub enum DNSError {
@@ -15,4 +16,37 @@ pub enum DNSError {
     ReservedOrUnassigned(QueryType),
 }
 
-// impl Error for DNSError {};
+impl fmt::Display for DNSError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let err_msg = match self {
+            Self::EndOfBuffer => "pointer of buffer is out of bounds".to_string(),
+            Self::BadPointerPosition => {
+                "DNS section is being accessed at the wrong position".to_string()
+            }
+            Self::UnknownResponseCode(code) => format!("wrong response code {}", code),
+            Self::NonUTF8 => "encountered non UTF-8 sequence".to_string(),
+            Self::MaxJumps => {
+                "exceeded max amount of jumps when processing compressed DNS label sequence"
+                    .to_string()
+            }
+            Self::LabelTooLarge(label, size) => format!(
+                "label {} of size {} exceeds maximum label length",
+                label, size
+            ),
+            Self::DomainNameTooLarge(domain, size) => format!(
+                "label {} of size {} exceeds maximum label length",
+                domain, size
+            ),
+            Self::UnimplementedRecordType(rtype) => {
+                format!("record of type {:?} has not been implemented", rtype)
+            }
+            Self::UnknownRecordWrite => "cannot write record of unknown type".to_string(),
+            Self::ReservedOrUnassigned(qtype) => {
+                format!("tried to write record of type {:?} containing fields which are currently reserved or unassigned", qtype)
+            }
+        };
+        write!(f, "{}", err_msg)
+    }
+}
+
+impl Error for DNSError {}
